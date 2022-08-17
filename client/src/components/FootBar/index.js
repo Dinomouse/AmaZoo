a04a021f9086cb816b3e275f1a578c90eaf70fc7
ArianMoossavi <arianmoossavi@live.co.uk>
2022-08-17 08:58:18 +0100

    css responsiveness to search page

--- client/src/components/FootBar/index.js
+++ client/src/components/FootBar/index.js
@@ -1,30 +1,23 @@
 import "./index.css";
 import { Link } from "react-router-dom";
-import zooIcon from "../../images/zooIcon.svg";
+
 import LoginButton from "../LoginButton/loginButton.js";
 import ProfileIcon from "../ProfileIcon/profileIcon.js";
 import LogoutButton from "../LogoutButton/logoutButton.js";
 import Basket from "../Basket";
-import About from "../../images/About.svg";
 
 function FootBar({ isAuthenticated, user, myBasket }) {
   return (
     <footer className="foot-bar">
       <div className="nav-buttons-container">
         <Link to="/About" style={{ textDecoration: "none" }}>
-          {/* <object
-          type={"image/svg+xml"}
-          data={About}
-          className="zoo-icon"
-          aria-label="about"
-        ></object> */}{" "}
           <div className="search">About</div>
         </Link>
         <Link to="/Search" style={{ textDecoration: "none" }}>
           <div className="search">Search</div>
         </Link>
         <Link to="/Zoo" style={{ textDecoration: "none" }}>
-          <div className="search">My Zoo</div>
+          <div className="search">Zoo</div>
         </Link>
       </div>
 
