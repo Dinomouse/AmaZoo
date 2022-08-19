3af707bbcd27fe4f6c359e884961248a5943511b
ArianMoossavi <arianmoossavi@live.co.uk>
2022-08-19 18:04:51 +0100

    copy on website amended

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,4 +1,3 @@
-import LoginButton from "../LoginButton/loginButton";
 import SignUp from "../SignUp";
 import "./homepage.css";
 
@@ -6,7 +5,7 @@ function HomePage({ isAuthenticated }) {
   return (
     <div className="home-page-container">
       <h1 className="main-title">Adopting animals made easy</h1>
-      {!isAuthenticated && <h3>Sign up and start saving live today:</h3>}
+      {!isAuthenticated && <h3>Sign up and start saving lives today:</h3>}
       {!isAuthenticated && <SignUp></SignUp>}
     </div>
   );
